930aa0f76ce337a5d191d1bb049cf073dcbb1983
Joshua Sonnikson <jsonnikson@gmail.com>
2011-11-26 09:53:55 -0800

    a few fixes

--- network.py
+++ network.py
@@ -22,27 +22,28 @@ class Network:
         self.window_middle = window_middle
         self.n_hidden_neurons = n_hidden_neurons
         self._init_layers()
-        self._pybrain_network = buildNetwork(self.n_input_neurons, self.n_hidden_neurons, self.n_output_neurons)
+        self.pybrain_network = buildNetwork(self.n_input_neurons, self.n_hidden_neurons, self.n_output_neurons)
         
     def _init_layers(self):
         # one neuron for each window/letter combination
         self.letter_neuron_names = list(product(corpus.all_letters, range(self.window_size)))
-        # mapping from (letter, pos) to input neuron index
-        self.letters_to_neurons = dict({(letter_and_pos, index) for index, letter_and_pos in enumerate(self.letter_neuron_names)})
         # one neuron for each phoneme trait
         self.phoneme_trait_neuron_names = list(corpus.all_phoneme_traits)
+        # neuron counts
+        self.n_input_neurons = len(self.letter_neuron_names)
+        self.n_output_neurons = len(self.phoneme_trait_neuron_names)        
+        # mapping from (letter, pos) to input neuron index
+        self.letters_to_neurons = dict({(letter_and_pos, index) for index, letter_and_pos in enumerate(self.letter_neuron_names)})
         # mapping from trait to neuron
         self.traits_to_neurons = dict({(trait, index) for index, trait in enumerate(self.phoneme_trait_neuron_names)})
         # mapping from phoneme to layer
         self.phonemes_to_layers = {}
         for (phoneme, traits) in corpus.phoneme_traits.iteritems():
-            layer = zeros(len(self.phoneme_trait_neuron_names))
+            layer = zeros(self.n_output_neurons)
             for trait in traits:
                 index = self.traits_to_neurons[trait]
                 layer[index] = 1
             self.phonemes_to_layers[phoneme] = layer
-        self.n_input_neurons = len(self.letter_neuron_names)
-        self.n_output_neurons = len(self.phoneme_trait_neuron_names)
 
     def windowIter(self, letters):
         assert type(letters) == str
@@ -56,19 +57,16 @@ class Network:
 
     def wordSamples(self, word):
         assert len(word.letters) == len(word.phonemes)
-        for (letters_window, current_phoneme) in izip(windowIter(word.letters), word.phonemes):
-            yield self._letters_to_layer(letters_window), phoneme_to_layer(current_phoneme)
-
-    def train(self, training_set):
-        _pybrain_network.train
+        for (letters_window, current_phoneme) in izip(self.windowIter(word.letters), word.phonemes):
+            yield self._letters_to_layer(letters_window), self.phoneme_to_layer(current_phoneme)
 
     def _letters_to_layer(self, letters):
         assert len(letters) == self.window_size
         # start with empty layer
-        layer = zeros(len(letter_neuron_names))
+        layer = zeros(self.n_input_neurons)
         # loop through letters and activate each neuron
         for (pos, letter) in enumerate(letters):
-            index = letters_to_neurons[(letter, pos)]
+            index = self.letters_to_neurons[(letter, pos)]
             layer[index] = 1
         return layer
 
