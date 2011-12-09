93bd8d83cf48eff92df97c25be9677b6cef1478f
Joshua Sonnikson <jsonnikson@gmail.com>
2011-12-08 17:07:57 -0800

    switch back to full dictionary

--- dict_test.py
+++ dict_test.py
@@ -5,6 +5,7 @@ import argparse
 import cPickle as pickle
 import scipy.io as io
 import corpus
+import sys
 
 def parseArgs():
     parser = argparse.ArgumentParser(description='Test NETwhisperer network against full dictionary.')
@@ -25,26 +26,31 @@ def main():
     correct_outputs = []
     produced_outputs = []
     angles = []
-    for w in corpus.top1000words:
+    for (i, w) in enumerate(corpus.dictionary.values()):
+        if not i % 1000:
+            print '%d..' % (i+1),
+            sys.stdout.flush()
+        
         res = network.lettersToPhonemesWithAngles(w.letters, w.phonemes)
         pph, angs = izip(*res)
         
-        inputs.extend(w.letters)
-        correct_outputs.extend(w.phonemes)
-        produced_outputs.extend(pph)
-        angles.extend(angs)
+        if args.matfile:
+            inputs.extend(w.letters)
+            correct_outputs.extend(w.phonemes)
+            produced_outputs.extend(pph)
+            angles.extend(angs)
         total += len(w.letters)
         correct += sum(int(a==b) for a,b in izip(pph, w.phonemes))
     
     percent_correct = 100.0 * correct / total
-    mdict = {
-        'percent_correct': percent_correct,
-        'inputs': inputs,
-        'correct_outputs': correct_outputs,
-        'produced_outputs': produced_outputs,
-        'angles': angles,  
-    }
-    if (args.matfile):
+    if args.matfile:
+        mdict = {
+            'percent_correct': percent_correct,
+            'inputs': inputs,
+            'correct_outputs': correct_outputs,
+            'produced_outputs': produced_outputs,
+            'angles': angles,  
+        }
         io.savemat(args.matfile, mdict, oned_as='row')
     print '%f%% correct' % percent_correct
     
